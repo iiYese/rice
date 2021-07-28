4a9e10e4fcc902cfa169a9add3a22d4c6838732c
iiYese <iiYese@outlook.com>
2021-07-28 19:44:02 +0100

    added a few key mappings

--- nvim/init.lua
+++ nvim/init.lua
@@ -509,3 +509,13 @@ vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', {noremap = true})
 vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', {noremap = true})
 vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', {noremap = true})
 vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', {noremap = true})
+
+
+vim.api.nvim_set_keymap('n', '<C-Up>', ':resize -1<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-Down>', ':resize +1<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-Left>', ':vertical resize -1<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-Right>', ':vertical resize +1<CR>', {noremap = true})
+
+
+vim.api.nvim_set_keymap('v', '<', '<gv', {noremap = true})
+vim.api.nvim_set_keymap('v', '>', '>gv', {noremap = true})
