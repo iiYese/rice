0ffca360632ac41549a511e954dc7c438f3d7779
iiYese <iiYese@outlook.com>
2021-07-31 17:50:17 +0100

    changed diagnostics update delay

--- nvim/init.lua
+++ nvim/init.lua
@@ -163,7 +163,7 @@ vim.g.termguicolors = true
         "LspDiagnosticsSignInformation",
         { texthl = "LspDiagnosticsSignInformation", text = "", numhl = "LspDiagnosticsSignInformation" }
     )
-    vim.o.updatetime = 0
+    vim.o.updatetime = 200
     vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.lsp.diagnostic.show_line_diagnostics({focusable=false})]]
     vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
         virtual_text = false,
