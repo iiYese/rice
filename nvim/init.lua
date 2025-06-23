44043ab4079cbc140c1939d4e41b7c33b19a2aae
iiyese <iiyese@outlook.com>
2025-06-23 23:23:38 +0100

    cooked rice

--- nvim/init.lua
+++ nvim/init.lua
@@ -77,26 +77,15 @@ require("lazy").setup({
     },
     {
       'saghen/blink.cmp',
-      -- optional: provides snippets for the snippet source
-      dependencies = { 'rafamadriz/friendly-snippets' },
-
       version = '1.*',
+      opts_extend = { "sources.default" },
       opts = {
         keymap = { preset = 'default' },
-
-        appearance = {
-          nerd_font_variant = 'mono'
-        },
-
+        appearance = { nerd_font_variant = 'mono' },
         completion = { documentation = { auto_show = false } },
-
-        sources = {
-          default = { 'lsp', 'path', 'snippets', 'buffer' },
-        },
-
+        sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
         fuzzy = { implementation = "prefer_rust_with_warning" }
       },
-      opts_extend = { "sources.default" }
     },
     { 'akinsho/toggleterm.nvim', version = "*", opts = { size = 100, direction = 'float' } },
     {
